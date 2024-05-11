b98569b905da401ee5b5f4debef39bddb148b675
Do Son Tung <tungds2001@gmail.com>
2024-05-10 20:19:38 -0400

    part 2

--- P2/test2.sh
+++ P2/test2.sh
@@ -3,5 +3,5 @@ source ../../env.sh
 
 /usr/local/hadoop/bin/hdfs dfs -rm -r /heart-disease/input/
 /usr/local/hadoop/bin/hdfs dfs -mkdir -p /heart-disease/input/
-/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../data/train.csv /heart-disease/input/
+/usr/local/hadoop/bin/hdfs dfs -copyFromLocal ../../data/framingham.csv /heart-disease/input/
 /usr/local/spark/bin/spark-submit --master=spark://$SPARK_MASTER:7077 part2.py hdfs://$SPARK_MASTER:9000/heart-disease/input/
